eda7eb2dca6567842082912f050930152f3b09f0
JhorviOlave <167636687+JhorviOlave@users.noreply.github.com>
2024-06-25 21:30:47 -0400

    Update Aplicacion.java

--- src/main/java/convertidormonedas/Aplicacion.java
+++ src/main/java/convertidormonedas/Aplicacion.java
@@ -1,26 +1,32 @@
-
 package convertidormonedas;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
-public class ApiConection {
-    
-    private HttpClient httpClient;
+public class Aplicacion {
 
-    public ApiConection() {
-        this.httpClient = HttpClient.newHttpClient();
+    private static ArrayList<Convertidor> listaConverticiones = new ArrayList<>();
+
+    public static void agregarRegistro(Convertidor registro) {
+        if (registro.getCodigoBase() != null && registro.getCodigoDestino() != null) {
+            listaConverticiones.add(registro);
+        }
     }
-    
-    public String getApiResponse(String url) throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        String apiResponse = response.body();
-        return apiResponse;
-    
+
+    public static void mostrarLista() {
+        for (Convertidor listaConverticione : listaConverticiones) {
+            System.out.println("Codigo Base: " + listaConverticione.getCodigoBase() +
+                    " |\tCodigo Destino: " + listaConverticione.getCodigoDestino() +
+                    "\t Cantidad: " + listaConverticione.getCantidadConvetir() +
+                    " |\tValor final: " + listaConverticione.getCantidadConvetida());
+        }
+    }
+
+    public static boolean tieneLista() {
+        return !Aplicacion.listaConverticiones.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "Aplicacion{" + "listaConverticiones=" + listaConverticiones + '}';
     }
 }
